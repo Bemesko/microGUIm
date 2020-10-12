90109b4da36da37c9d01eefb726cd1b5b9a8bef4
Bernardo Mesko <bemesko@gmail.com>
2020-10-12 15:27:17 -0300

    Started refactoring get_bids()

--- ioh_backend.py
+++ ioh_backend.py
@@ -32,16 +32,16 @@ class auction_sync(Agent):
     def send_market_prices(self):
         self.current_market_prices = random.randrange(1, 100)
         self.send('marketPrices', self.current_market_prices)
-        self.log_info('Market Prices Sent! ' +
-                      str(self.current_market_prices))
+        self.log_info(
+            f"Market prices sent! Current price: {self.current_market_prices}")
 
     def gather_sellers(self):
-        self.seler_agents = []
+        self.seller_agents = []
         for i in range(2):
-            address_alias = 'requestSeller' + str(i)
+            address_alias = f"requestSeller{i}"
             self.send(address_alias, 'Will you sell?')
             if self.recv(address_alias):
-                self.seler_agents.append(i)
+                self.seller_agents.append(i)
         self.log_info('Sellers Gathered!')
 
     def auction(self):
@@ -54,21 +54,21 @@ class prosumer(Agent):
     predictions_at = 18
     calculations_at = 21
     store_data = True
-    buying_parameters = {
+    buy_parameters = {
         constants.BASELINE: buy_baseline.deficit,
         constants.ENERGY: 80,
         constants.MAX_PRICE: 90,
         constants.START_PRICE: 30,
         constants.INCREMENT: 10,
     }
-    selling_parameters = {
+    sell_parameters = {
         constants.BASELINE: sell_baseline.surplus,
         constants.ENERGY: 80,
         constants.MIN_PRICE: 110,
         constants.MAX_LOT_SIZE: 100
     }
     is_seller = False
-    energy_differential = 0
+    energy_difference = 0
     energy_market_price = 0
     wanted_energy = 0
     energy_buying_max_price = 0
@@ -80,49 +80,50 @@ class prosumer(Agent):
     def predict_energy(self):
         self.next_energy_consumption = random.randrange(1, 100)
         self.next_energy_generation = random.randrange(1, 100)
-        self.log_info('Energy Predicted to ' + str(self.next_energy_consumption) +
-                      'Con, Gen' + str(self.next_energy_generation))
+        self.log_info(
+            f"Predicted Consumption: {self.next_energy_consumption}; Generation: {self.next_energy_generation}")
 
     def get_bids(self):
-        self.energy_differential = self.next_energy_generation - self.next_energy_consumption
-        if self.energy_differential >= 0:
-            self.is_seller = False
-            if(self.buying_parameters['baseline'] == 'none'):
+        self.energy_difference = self.next_energy_generation - self.next_energy_consumption
+        self.is_seller = not(self.energy_difference >= 0)
+        if self.is_seller:
+            self.energy_difference *= -1
+            self.wanted_energy = self.calculate_sell_energy(
+                self.sell_parameters[constants.BASELINE])
+            self.log_info(f"Will sell! Wanted energy: {self.wanted_energy}")
+        else:
+            if(self.buy_parameters['baseline'] == 'none'):
                 self.wanted_energy = 0
             else:
-                if(self.buying_parameters['baseline'] == 'deficit'):
-                    self.wanted_energy = self.energy_differential
-                elif(self.buying_parameters['baseline'] == 'all'):
+                if(self.buy_parameters['baseline'] == 'deficit'):
+                    self.wanted_energy = self.energy_difference
+                elif(self.buy_parameters['baseline'] == 'all'):
                     self.wanted_energy = self.next_energy_consumption
-                elif(self.buying_parameters['baseline'] == 'infinite'):
+                elif(self.buy_parameters['baseline'] == 'infinite'):
                     self.wanted_energy = 99999
                 self.wanted_energy = self.wanted_energy * \
-                    self.buying_parameters['energy'] / 100
+                    self.buy_parameters['energy'] / 100
             self.log_info('Will Buy' + str(self.wanted_energy))
-        else:
-            self.is_seller = True
-            self.energy_differential = self.energy_differential * -1
-            if(self.selling_parameters['baseline'] == 'none'):
-                self.wanted_energy = 0
-            else:
-                if(self.selling_parameters['baseline'] == 'surplus'):
-                    self.wanted_energy = self.energy_differential
-                elif(self.selling_parameters['baseline'] == 'all'):
-                    self.wanted_energy = self.next_energy_generation
-                self.wanted_energy = self.wanted_energy * \
-                    self.selling_parameters['energy'] / 100
-            self.log_info('Will Sell' + str(self.wanted_energy))
+
+    def calculate_sell_energy(self, baseline):
+        sell_energy = 0
+        if(baseline == sell_baseline.all_energy):
+            sell_energy = self.next_energy_generation
+        elif(baseline == sell_baseline.surplus):
+            sell_energy = self.energy_difference
+        sell_energy *= self.sell_parameters[constants.ENERGY]/100
+        return sell_energy
 
     def get_market_prices(self, message):
         self.energy_market_price = int(message)
         self.energy_buying_starting_price = self.energy_market_price * \
-            self.buying_parameters['starting_price'] / 100
+            self.buy_parameters['starting_price'] / 100
         self.energy_buy_price_increment = self.energy_market_price * \
-            self.buying_parameters['increment'] / 100
+            self.buy_parameters['increment'] / 100
         self.energy_buying_max_price = self.energy_market_price * \
-            self.buying_parameters['max_price'] / 100
+            self.buy_parameters['max_price'] / 100
         self.energy_selling_min_price = self.energy_market_price * \
-            self.selling_parameters['min_price'] / 100
+            self.sell_parameters['min_price'] / 100
         self.log_info(
             f"Prices gathered! Market: {self.energy_market_price}; Buy Start: {self.energy_buying_starting_price}; Buy Increment: {self.energy_buy_price_increment}; Buy Max: {self.energy_buying_max_price}; Sell Min: {self.energy_selling_min_price}")
 
@@ -134,14 +135,14 @@ if __name__ == '__main__':
     agent_amount = int(input('How many agents?'))
     prosumers = []
 
-    '''Setup Agentes'''
+    '''Agent Setup'''
     ns = run_nameserver()
     auction_sync_agent = run_agent('auction_sync', base=auction_sync)
     for i in range(agent_amount):
-        agent_name = 'prosumer' + str(i)
+        agent_name = f"Prosumer{i}"
         prosumers.append(run_agent(agent_name, base=prosumer))
 
-    '''Setup Comunicações'''
+    '''Communications Setup'''
     requested_seller_addresses = []
     for i in range(agent_amount):
         addrAlias = 'requestSeller' + str(i)
