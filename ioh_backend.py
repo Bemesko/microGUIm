b9e1e9a92df8c5a25fff7ea95423f81b3b05a208
Bernardo Mesko <bemesko@gmail.com>
2020-11-16 21:31:09 -0300

    Multiple graphs implemented

--- ioh_backend.py
+++ ioh_backend.py
@@ -49,7 +49,7 @@ class Prosumer(Agent):
         self.energy_market_price = 0
         self.wanted_energy = 0
         self.energy_buy_max_price = 0
-        self.energy_buy_starting_price = 0
+        self.energy_buy_start_price = 0
         self.energy_buy_price_increment = 0
         self.energy_sell_min_price = 0
         self.buy_parameters = {
@@ -67,23 +67,36 @@ class Prosumer(Agent):
         }
 
         self.all_energy_consumption = []
+        self.all_energy_generation = []
+        self.all_energy_difference = []
+        self.all_energy_market_prices = []
+        self.all_wanted_energy = []
+        self.all_energy_buy_max_prices = []
+        self.all_energy_buy_start_prices = []
+        self.all_energy_buy_price_increments = []
+        self.all_energy_sell_min_prices = []
 
     def predict_energy(self):
         self.next_energy_consumption = random.randrange(1, 100)
 
-        # O código abaixo é uma gambiarra imensa. Deus, por favor, me perdoe.
-        if(len(self.all_energy_consumption) < constants.ATTRIBUTE_LIST_LENGTH):
-            self.all_energy_consumption.append(self.next_energy_consumption)
-        else:
-            for i in range(len(self.all_energy_consumption)):
-                if i > 0:
-                    self.all_energy_consumption[i -
-                                                1] = self.all_energy_consumption[i]
-            self.all_energy_consumption[-1] = self.next_energy_consumption
+        self.add_value_to_limited_list(
+            self.next_energy_consumption, self.all_energy_consumption)
 
         self.next_energy_generation = random.randrange(1, 100)
+
+        self.add_value_to_limited_list(
+            self.next_energy_generation, self.all_energy_generation)
         self.log_info(
             f"Predicted Consumption: {self.next_energy_consumption}; Generation: {self.next_energy_generation}")
+
+    def add_value_to_limited_list(self, value, list_):
+        if(len(list_) < constants.ATTRIBUTE_LIST_LENGTH):
+            list_.append(value)
+        else:
+            for i in range(len(list_)):
+                if i > 0:
+                    list_[i - 1] = list_[i]
+            list_[-1] = value
 
     def get_bids(self):
         self.energy_difference = self.next_energy_generation - self.next_energy_consumption
@@ -97,21 +110,24 @@ class Prosumer(Agent):
             self.wanted_energy = self.calculate_buy_energy()
             self.log_info(f"Will Buy {self.wanted_energy}")
 
+        self.add_value_to_limited_list(
+            self.wanted_energy, self.all_wanted_energy)
+
     def calculate_buy_energy(self):
         baseline = self.buy_parameters[constants.BASELINE]
-        sell_energy = 0
+        buy_energy = 0
 
         if(baseline == constants.buy_baseline.deficit):
-            sell_energy = self.energy_difference
+            buy_energy = self.energy_difference
         elif(baseline == constants.buy_baseline.all_energy):
-            sell_energy = self.next_energy_consumption
+            buy_energy = self.next_energy_consumption
         elif(baseline == constants.buy_baseline.infinite):
-            sell_energy = 99999
+            buy_energy = 99999
         else:
-            sell_energy = 0
+            buy_energy = 0
 
-        sell_energy *= self.buy_parameters[constants.ENERGY] / 100
-        return sell_energy
+        buy_energy *= self.buy_parameters[constants.ENERGY] / 100
+        return buy_energy
 
     def calculate_sell_energy(self):
         baseline = self.sell_parameters[constants.BASELINE]
@@ -129,9 +145,9 @@ class Prosumer(Agent):
 
     def get_market_prices(self, message):
         market_price = int(message)
-        self.energy_market_price = int(message)
+        self.energy_market_price = market_price
 
-        self.energy_buy_starting_price = market_price * \
+        self.energy_buy_start_price = market_price * \
             self.buy_parameters[constants.START_PRICE] / 100
         self.energy_buy_price_increment = self.energy_market_price * \
             self.buy_parameters[constants.INCREMENT] / 100
@@ -139,8 +155,20 @@ class Prosumer(Agent):
             self.buy_parameters[constants.MAX_PRICE] / 100
         self.energy_sell_min_price = self.energy_market_price * \
             self.sell_parameters[constants.MIN_PRICE] / 100
+
+        self.add_value_to_limited_list(
+            self.energy_market_price, self.all_energy_market_prices)
+        self.add_value_to_limited_list(
+            self.energy_buy_start_price, self.all_energy_buy_start_prices)
+        self.add_value_to_limited_list(
+            self.energy_buy_price_increment, self.all_energy_buy_price_increments)
+        self.add_value_to_limited_list(
+            self.energy_buy_max_price, self.all_energy_buy_max_prices)
+        self.add_value_to_limited_list(
+            self.energy_sell_min_price, self.all_energy_sell_min_prices)
+
         self.log_info(
-            f"Prices gathered! Market: {self.energy_market_price}; Buy Start: {self.energy_buy_starting_price}; Buy Increment: {self.energy_buy_price_increment}; Buy Max: {self.energy_buy_max_price}; Sell Min: {self.energy_sell_min_price}")
+            f"Prices gathered! Market: {self.energy_market_price}; Buy Start: {self.energy_buy_start_price}; Buy Increment: {self.energy_buy_price_increment}; Buy Max: {self.energy_buy_max_price}; Sell Min: {self.energy_sell_min_price}")
 
     def answer_sell_request(self, message):
         return self.is_seller
@@ -214,6 +242,22 @@ class MultiagentSystem():
         for prosumer in self.prosumers:
             self.agent_attributes[constants.NEXT_ENERGY_CONSUMPTION][i] = prosumer.get_attr(
                 "all_energy_consumption")
+            self.agent_attributes[constants.NEXT_ENERGY_GENERATION][i] = prosumer.get_attr(
+                "all_energy_generation")
+            self.agent_attributes[constants.ENERGY_DIFFERENCE][i] = prosumer.get_attr(
+                "all_energy_difference")
+            self.agent_attributes[constants.ENERGY_MARKET_PRICE][i] = prosumer.get_attr(
+                "all_energy_market_prices")
+            self.agent_attributes[constants.WANTED_ENERGY][i] = prosumer.get_attr(
+                "all_wanted_energy")
+            self.agent_attributes[constants.ENERGY_BUY_MAX_PRICE][i] = prosumer.get_attr(
+                "all_energy_buy_max_prices")
+            self.agent_attributes[constants.ENERGY_BUY_STARTING_PRICE][i] = prosumer.get_attr(
+                "all_energy_buy_start_prices")
+            self.agent_attributes[constants.ENERGY_BUY_PRICE_INCREMENT][i] = prosumer.get_attr(
+                "all_energy_buy_price_increments")
+            self.agent_attributes[constants.ENERGY_SELL_MIN_PRICE][i] = prosumer.get_attr(
+                "all_energy_sell_min_prices")
             i += 1
 
     def shutdown(self):
--- window_layout.py
+++ window_layout.py
@@ -41,8 +41,32 @@ class tkinterApp(tk.Tk):
             self.frame_buttons, text="Next Generation", command=lambda: self.show_frame(PageGraphNextGeneration))
         self.button_graph_next_generation.grid(sticky="we")
 
+        self.button_graph_market_price = tk.Button(
+            self.frame_buttons, text="Market Prices", command=lambda: self.show_frame(PageGraphMarket))
+        self.button_graph_market_price.grid(sticky="we")
+
+        self.button_graph_wanted_energy = tk.Button(
+            self.frame_buttons, text="Wanted Energy", command=lambda: self.show_frame(PageGraphWanted))
+        self.button_graph_wanted_energy.grid(sticky="we")
+
+        self.button_graph_max_price = tk.Button(
+            self.frame_buttons, text="Highest Bid to Buy", command=lambda: self.show_frame(PageGraphMaxPrice))
+        self.button_graph_max_price.grid(sticky="we")
+
+        self.button_graph_start_price = tk.Button(
+            self.frame_buttons, text="Lowest Bid to Buy", command=lambda: self.show_frame(PageGraphStartPrice))
+        self.button_graph_start_price.grid(sticky="we")
+
+        self.button_graph_price_increment = tk.Button(
+            self.frame_buttons, text="Bid Price Increment", command=lambda: self.show_frame(PageGraphPriceIncrement))
+        self.button_graph_price_increment.grid(sticky="we")
+
+        self.button_graph_min_price = tk.Button(
+            self.frame_buttons, text="Lowest Bid to Sell", command=lambda: self.show_frame(PageGraphMinPrice))
+        self.button_graph_min_price.grid(sticky="we")
+
         self.button_info = tk.Button(
-            self.frame_buttons, text="Info", command=lambda: self.show_frame(PageAgentInfo))
+            self.frame_buttons, text="Agent Info", command=lambda: self.show_frame(PageAgentInfo))
         self.button_info.grid(sticky="we")
 
         self.frame_middle = tk.Frame(self)
@@ -59,11 +83,12 @@ class tkinterApp(tk.Tk):
 
         self.frames = {}
 
-        for new_frame in (PageSystemInfo, PageGraphNextConsumption, PageGraphNextGeneration, PageAgentInfo):
+        for new_frame in (PageSystemInfo, PageGraphNextConsumption, PageGraphNextGeneration, PageGraphDifference, PageGraphMarket, PageGraphWanted, PageGraphMaxPrice, PageGraphStartPrice, PageGraphPriceIncrement, PageGraphMinPrice, PageAgentInfo):
 
             frame = new_frame(self.frame_middle, self, self.mas)
 
             self.frames[new_frame] = frame
+            print(frame.is_visible)
 
             frame.grid(row=0, column=0, sticky="nswe", padx=15, pady=15)
 
@@ -74,7 +99,9 @@ class tkinterApp(tk.Tk):
         frame = self.frames[desired_frame]
         frame.is_visible = True
         frame.tkraise()
-        self.last_frame = desired_frame
+        print(self.last_frame.is_visible)
+        self.last_frame = frame
+        print(frame.is_visible)
 
     def on_closing(self):
         if messagebox.askokcancel("Quit", "Are you sure you want to quit?"):
@@ -216,6 +243,90 @@ class PageGraphNextGeneration(PageGraph):
     def update_graph(self, attribute, title, xlabel, ylabel):
         PageGraph.update_graph(self, constants.NEXT_ENERGY_GENERATION,
                                "Next Energy Generation", "System Cycles", "Predicted Energy Generation (W)")
+
+
+class PageGraphDifference(PageGraph):
+    def __init__(self, parent, controller, multiagent_system, *args, **kwargs):
+        PageGraph.__init__(self, parent, controller,
+                           multiagent_system, *args, **kwargs)
+        self.update_graph(constants.ENERGY_DIFFERENCE, "Energy Difference",
+                          "System Cycles", "Energy Difference (W)")
+
+    def update_graph(self, attribute, title, xlabel, ylabel):
+        PageGraph.update_graph(self, constants.ENERGY_DIFFERENCE,
+                               "Energy Difference", "System Cycles", "Energy Difference (W)")
+
+
+class PageGraphMarket(PageGraph):
+    def __init__(self, parent, controller, multiagent_system, *args, **kwargs):
+        PageGraph.__init__(self, parent, controller,
+                           multiagent_system, *args, **kwargs)
+        self.update_graph(constants.ENERGY_MARKET_PRICE, "Energy Market Price",
+                          "System Cycles", "Energy Market Prices ($)")
+
+    def update_graph(self, attribute, title, xlabel, ylabel):
+        PageGraph.update_graph(self, constants.ENERGY_MARKET_PRICE,
+                               "Energy Market Price", "System Cycles", "Energy Market Prices ($)")
+
+
+class PageGraphWanted(PageGraph):
+    def __init__(self, parent, controller, multiagent_system, *args, **kwargs):
+        PageGraph.__init__(self, parent, controller,
+                           multiagent_system, *args, **kwargs)
+        self.update_graph(constants.WANTED_ENERGY, "Wanted Energy",
+                          "System Cycles", "Wanted Energy by Agent (W)")
+
+    def update_graph(self, attribute, title, xlabel, ylabel):
+        PageGraph.update_graph(self, constants.WANTED_ENERGY,
+                               "Wanted Energy", "System Cycles", "Wanted Energy by Agent (W)")
+
+
+class PageGraphMaxPrice(PageGraph):
+    def __init__(self, parent, controller, multiagent_system, *args, **kwargs):
+        PageGraph.__init__(self, parent, controller,
+                           multiagent_system, *args, **kwargs)
+        self.update_graph(constants.ENERGY_BUY_MAX_PRICE, "Highest Price to Buy Energy",
+                          "System Cycles", "Highest Price ($)")
+
+    def update_graph(self, attribute, title, xlabel, ylabel):
+        PageGraph.update_graph(self, constants.ENERGY_BUY_MAX_PRICE,
+                               "Highest Price to Buy Energy", "System Cycles", "Highest Price ($)")
+
+
+class PageGraphStartPrice(PageGraph):
+    def __init__(self, parent, controller, multiagent_system, *args, **kwargs):
+        PageGraph.__init__(self, parent, controller,
+                           multiagent_system, *args, **kwargs)
+        self.update_graph(constants.ENERGY_BUY_STARTING_PRICE, "Lowest Price to Buy Energy",
+                          "System Cycles", "Lowest Price to Buy Energy ($)")
+
+    def update_graph(self, attribute, title, xlabel, ylabel):
+        PageGraph.update_graph(self, constants.ENERGY_BUY_STARTING_PRICE,
+                               "Lowest Price to Buy Energy", "System Cycles", "Lowest Price to Buy Energy ($)")
+
+
+class PageGraphPriceIncrement(PageGraph):
+    def __init__(self, parent, controller, multiagent_system, *args, **kwargs):
+        PageGraph.__init__(self, parent, controller,
+                           multiagent_system, *args, **kwargs)
+        self.update_graph(constants.ENERGY_BUY_PRICE_INCREMENT, "Bid Price Increment",
+                          "System Cycles", "Bid Increment ($)")
+
+    def update_graph(self, attribute, title, xlabel, ylabel):
+        PageGraph.update_graph(self, constants.ENERGY_BUY_PRICE_INCREMENT,
+                               "Bid Price Increment", "System Cycles", "Bid Increment ($)")
+
+
+class PageGraphMinPrice(PageGraph):
+    def __init__(self, parent, controller, multiagent_system, *args, **kwargs):
+        PageGraph.__init__(self, parent, controller,
+                           multiagent_system, *args, **kwargs)
+        self.update_graph(constants.ENERGY_SELL_MIN_PRICE, "Lowest Price to Sell Energy",
+                          "System Cycles", "Lowest Price to Sell ($)")
+
+    def update_graph(self, attribute, title, xlabel, ylabel):
+        PageGraph.update_graph(self, constants.ENERGY_SELL_MIN_PRICE,
+                               "Lowest Price to Sell Energy", "System Cycles", "Lowest Price to Sell ($)")
 
 
 class PageAgentInfo(tk.Frame):
